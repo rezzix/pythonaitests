e10c6d8af1df7123e7541dd6dc642a5edc45d6ce
ossama <ossama@localhost.localdomain>
2019-11-20 19:21:58 +0100

    mastermid 2 resolvers ready

--- mastermind.py
+++ mastermind.py
@@ -26,7 +26,6 @@ class Code:
             #print ("{} gess {} code {}".format(valx, guess[valx], self.code[valx]))
             if  guess[valx] == self.code[valx] :
                 eval.inplace += 1
-                print "in place"
                 continue
             
             if guess[valx] in self.code :
@@ -35,23 +34,102 @@ class Code:
         return eval
     def display(self):
         print (self.code)
+    def equals(self, othercode):
+        for indx in range(len(self.code)) :
+            if self.code[indx] != othercode.code[indx]:
+                return False
+        return True
 
 
 class Evaluation:
     def __init__(self):
         self.inplace = 0
         self.existing = 0
-    
+
+    def perfect(self) :
+        if self.inplace == 4 :
+            return True
+        else :
+            return False
+
+    def equals(self, otherevaluation):
+        if (self.inplace==otherevaluation.inplace and self.existing==otherevaluation.existing):
+            return True
+        else:
+            return False    
     def display(self):
         print ("Inplace : {}, Existing : {}".format(self.inplace, self.existing))
 
+
+# Stupid solver only uses memory, it will not give the guess twice
+class StupidSolver:
+    def solve(self, code):
+        guesses = []
+        evaluations = []
+        eval = Evaluation()
+
+        while not eval.perfect() :
+            guess = Code([])
+            guess.randomize(4,colors)
+            if any(guess.equals(g) for g in guesses):
+                continue
+            guesses.append(guess)
+            if len(guesses)==6**4:
+                break
+            eval = code.evaluate(guess.code)
+            evaluations.append(eval)
+
+        if eval.perfect():
+            print("perfect code using {} guesses".format(len(guesses)))
+        else:
+            print("not found using {} guesses".format(len(guesses)))
+        guesses[len(guesses)-1].display() 
+
+# Verifier solver will check against available results before making a guess
+class VerifierSolver:
+    def solve(self, code):
+        guesses = []
+        badguesses = []
+        evaluations = []
+        eval = Evaluation()
+
+        while len(guesses) + len(badguesses) <= 6**4 and not eval.perfect() :
+            guess = Code([])
+            guess.randomize(4,colors)
+            if any(guess.equals(g) for g in guesses):
+                continue
+            if any(guess.equals(g) for g in badguesses):
+                continue
+            # good guess based on history
+            goodguess = True
+            for indx in range(len(guesses)):
+                if not guess.evaluate(guesses[indx].code).equals(evaluations[indx]):
+                    goodguess = False
+                    break
+            if not goodguess :
+                badguesses.append(guess)
+                continue
+            guesses.append(guess)
+            eval = code.evaluate(guess.code)
+            evaluations.append(eval)
+            guess.display()
+            eval.display()
+
+        if eval.perfect():
+            print("perfect code using {} guesses and skipping {}".format(len(guesses), len(badguesses)))
+        else:
+            print("not found using {} guesses and skipping {}".format(len(guesses), len(badguesses)))
+        guesses[len(guesses)-1].display()     
+
 #secretcode = Code({colors[randrange(0, 6)],colors[randrange(0, 6)],colors[randrange(0, 6)],colors[randrange(0, 6)]]})
 secretcode = Code(['red', 'yellow', 'orange', 'orange'])
-
 secretcode.display()
 
-#secretcode.evaluate(['red', 'orange', 'blue', 'yellow']).display()
-for indx in range(10) :
-    guess = Code([])
-    guess.randomize(4,colors)
-    secretcode.evaluate(guess).display()
+print('\nStupid solver processing : \n')
+solver1 = StupidSolver()
+solver1.solve(secretcode)
+
+print('\nVerifier solver processing : \n')
+solver2 = VerifierSolver()
+solver2.solve(secretcode)
+
